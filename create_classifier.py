b43b6dba5cbfd097d0c364c7dc9dc2bd7c5541e1
mtyszler <tyszler.jobs@gmail.com>
2020-05-05 14:35:03 +0200

    Insert missing package

--- create_classifier.py
+++ create_classifier.py
@@ -4,6 +4,7 @@ both train and predict use the same classifier architecture
 '''
 
 from torch import nn
+from torchvision import models
 
 # the function below is adapted from:
 # https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html
